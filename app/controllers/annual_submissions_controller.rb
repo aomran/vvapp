4f492ae57a140c59767885200f811d441d67edf8
Ahmed Omran <aomra015@gmail.com>
2013-07-30 23:00:40 -0400

    code to pass integration test - artist can make an annual submission

--- app/controllers/annual_submissions_controller.rb
+++ app/controllers/annual_submissions_controller.rb
@@ -1,2 +1,22 @@
 class AnnualSubmissionsController < ApplicationController
+
+  def index
+
+  end
+
+  def new
+    @annual_submission = AnnualSubmission.new
+  end
+
+  def create
+    @annual_submission = AnnualSubmission.new(annual_submission_params)
+
+    @annual_submission.save
+    redirect_to annual_submissions_path, notice: "Your Submission has been received!"
+  end
+
+  private
+  def annual_submission_params
+    params.require(:annual_submission).permit(:cv, :artist_statement, :expo_project, :special_needs, :image_list)
+  end
 end
--- test/integration/annual_submissions_test.rb
+++ test/integration/annual_submissions_test.rb
@@ -3,17 +3,17 @@ require 'test_helper'
 class AnnualSubmissionsTest < Capybara::Rails::TestCase
   test "artist can make an annual submission" do
     visit new_annual_submission_path
-    fill_in :cv, with: 'cv.pdf'
-    fill_in :artist_statement, with: 'artist_statement.pdf'
-    fill_in :cv, with: 'expo_project.pdf'
-    fill_in :cv, with: 'special_needs.pdf'
-    fill_in :cv, with: 'image_list.pdf'
+    fill_in "CV", with: 'cv.pdf'
+    fill_in "Artist Statement", with: 'artist_statement.pdf'
+    fill_in "Expo Project", with: 'expo_project.pdf'
+    fill_in "Special Needs", with: 'special_needs.pdf'
+    fill_in "Image List", with: 'image_list.pdf'
 
 
     assert_difference 'AnnualSubmission.count' do
-      click_button 'Submit'
+      click_button 'Create Annual submission'
     end
 
-    assert page.has_content?("Your Submission has been received!")
+    assert page.has_content?("Your Submission has been received!"), 'Notice about submission received not shown'
   end
 end
