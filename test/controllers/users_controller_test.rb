34252297ccb0e88684bad17d232cba2dc0750e3c
Ahmed O <aomra015@gmail.com>
2013-07-31 16:16:50 -0400

    add list of users to index view

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -8,4 +8,10 @@ class UsersControllerTest < ActionController::TestCase
     assert assigns(:user)
     assert :success
   end
+
+  test "should get list of all users" do
+    get :index
+    assert assigns(:users), 'Collection of users not passed to view'
+    assert :success
+  end
 end
