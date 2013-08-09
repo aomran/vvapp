e3717cd478e2663603d047407f1067dc56a2a2f6
Paula_Franzini <pjfranzini@hotmail.com>
2013-08-09 11:58:56 -0400

    tests fixed to match french keywords

--- test/integration/artist_user_accounts_test.rb
+++ test/integration/artist_user_accounts_test.rb
@@ -4,21 +4,21 @@ class ArtistUserAccountsTest < Capybara::Rails::TestCase
 
   test "an artist can signup for an account" do
     visit register_path
-    fill_in 'First name', with: 'Paula'
-    fill_in 'Last name', with: 'LittlyFrenchy'
-    fill_in 'Email', with: 'paula@littlefrance.com'
-    fill_in 'Password', with: 'password123'
-    fill_in 'Password confirmation', with: 'password123'
-    fill_in 'Gender', with: 'Female'
-    fill_in 'Phone number', with: '(819)667-3467'
-    fill_in 'Street address', with: '1234 Something dr.'
-    fill_in 'City', with: 'Gatineau'
+    fill_in 'Prénom', with: 'Paula'
+    fill_in 'Nom de famille', with: 'LittlyFrenchy'
+    fill_in 'Courriel', with: 'paula@littlefrance.com'
+    fill_in 'Mot de passe', with: 'password123'
+    fill_in 'Confirmez mot de passe', with: 'password123'
+    fill_in 'Numéro de téléphone', with: '(819)667-3467'
+    fill_in 'Adresse de rue', with: '1234 Something dr.'
+    fill_in 'Ville', with: 'Gatineau'
     fill_in 'Province', with: 'Quebec'
-    fill_in 'Postal code', with: 'J8G5H6'
-    fill_in 'Country', with: 'Quebec'
+    fill_in 'Code Postal', with: 'J8G5H6'
+    fill_in 'Pays', with: 'Quebec'
+    fill_in 'Femme/Homme radio', with: 'Female'
 
     assert_difference 'User.count' do
-      click_button 'Create User'
+      click_button 'Inscrivez-vous'
     end
 
     user = User.last
--- test/integration/user_sessions_test.rb
+++ test/integration/user_sessions_test.rb
@@ -5,9 +5,9 @@ class UserSessionsTest < Capybara::Rails::TestCase
   test "artists can login" do
     visit login_path
 
-    fill_in 'Email', with: users(:paula).email
-    fill_in 'Password', with: 'password123'
-    click_button 'Log In'
+    fill_in 'Courriel', with: users(:paula).email
+    fill_in 'Mot de passe', with: 'password123'
+    click_button 'Connexion'
 
     assert page.has_content?('You are logged in as paula@Lilfrenchy.com'), 'Email of logged in user is not shown'
     assert current_path == user_path(users(:paula))
@@ -16,9 +16,9 @@ class UserSessionsTest < Capybara::Rails::TestCase
   test "artists can't login with wrong password" do
     visit login_path
 
-    fill_in 'Email', with: users(:paula).email
-    fill_in 'Password', with: 'wrongpassword'
-    click_button 'Log In'
+    fill_in 'Courriel', with: users(:paula).email
+    fill_in 'Mot de passe', with: 'wrongpassword'
+    click_button 'Connexion'
 
     assert page.has_content?('Email or password is invalid.'), 'Error message is not shown'
 
