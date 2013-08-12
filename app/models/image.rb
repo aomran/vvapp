447d1fe7d4757b56236fed6c75f558371f8deef0
Ahmed O <aomra015@gmail.com>
2013-08-12 19:23:18 -0400

    filenames of images are now displayed as well as the thumbnails

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,4 +2,8 @@ class Image < ActiveRecord::Base
   belongs_to :submission
 
   mount_uploader :image_file, ImageUploader
+
+  def filename
+    File.basename(image_file.url, '.*').titleize
+  end
 end
