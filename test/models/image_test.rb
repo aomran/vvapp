3218ebe681ece5e69da35bcddf697b7f2500efc5
Ahmed O <aomra015@gmail.com>
2013-08-13 14:54:01 -0400

    non jpg images can not be uploaded

--- test/models/image_test.rb
+++ test/models/image_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class ImageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "non jpeg/jpg images are not stored" do
+    image = Image.new
+    uploader = ImageUploader.new(image, :image_file)
+
+    assert_raises CarrierWave::IntegrityError do
+    uploader.store!(File.open("#{Rails.root}/test/fixtures/images/not_a_jpg.png"))
+    end
+  end
 end
