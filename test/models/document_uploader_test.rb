2831775cc4d5e89b63f6f789d79c832f406f74e4
Paula_Franzini <pjfranzini@hotmail.com>
2013-08-17 12:02:31 -0400

    non pdf/doc documents are not stored

---
+++ test/models/document_uploader_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class DocumentUploaderTest < ActiveSupport::TestCase
+  test "non pdf/doc documents are not stored" do
+    submission = Submission.new
+    uploader = DocumentUploader.new(submission, :cv)
+
+    assert_raises CarrierWave::IntegrityError do
+      uploader.store!(File.open("#{Rails.root}/test/fixtures/documents/typeTest.pages"))
+    end
+
+    assert_nothing_raised CarrierWave::IntegrityError do
+      uploader.store!(File.open("#{Rails.root}/test/fixtures/documents/typeTest.pdf"))
+    end
+
+    assert_nothing_raised CarrierWave::IntegrityError do
+      uploader.store!(File.open("#{Rails.root}/test/fixtures/documents/typeTest.doc"))
+    end
+  end
+end
