88c547454ed3b3c88fbe354aae2eef95326bc80a
Ahmed Omran <aomra015@gmail.com>
2013-09-09 18:11:57 -0400

    fixed email test

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -4,10 +4,10 @@ class UserMailerTest < ActionMailer::TestCase
   test "password_reset" do
 
     mail = users(:bob).send_password_reset
-    assert_equal "Mettre à jour votre mot de passe", mail.subject
+    assert_equal "Comment mettre à jour votre mot de passe", mail.subject
     assert_equal [users(:bob).email], mail.to
     assert_equal ["voixvisuelle@gmail.com"], mail.from
-    assert_match "Pour mettre à jour votre mot de passe cliquer le lien ci-dessous.", mail.body.encoded
+    assert_match "Pour mettre à jour votre mot de passe veuillez cliquer le lien ci-dessous.", mail.body.encoded
   end
 
 end
